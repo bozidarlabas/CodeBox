76d03d5228de0731e707f1b009a3a8fc3981d69b
Bozidar Labas <bozidar.labas@hotmail.com>
2015-03-20 21:03:35 +0100

    Navigation Drawer

--- app/src/main/java/com/labas/bozidar/foi/materialdesignexample/MainActivity.java
+++ app/src/main/java/com/labas/bozidar/foi/materialdesignexample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.labas.bozidar.foi.materialdesignexample;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -15,10 +16,18 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_appbar);
 
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
+        //Set display enabled so user can slide drawer from left
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //Initialize the DrawerLayout
+        NavigationDrawerFragment drawerFreagment =
+                (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFreagment.setup(R.id.fragment_navigation_drawer, (DrawerLayout)findViewById(R.id.drawer_layout), toolbar);
 
     }
 
