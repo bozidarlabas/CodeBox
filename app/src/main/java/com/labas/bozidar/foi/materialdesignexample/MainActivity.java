bd56e79273619bc5d090ed77e41fc746b87030bf
Bozidar Labas <bozidar.labas@hotmail.com>
2015-03-21 13:41:34 +0100

    Translucent Status Bar

--- app/src/main/java/com/labas/bozidar/foi/materialdesignexample/MainActivity.java
+++ app/src/main/java/com/labas/bozidar/foi/materialdesignexample/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_appbar);
+        setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
